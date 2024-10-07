01055a99ee3c7442deec049a9b4f9b0fe70bc392
innuendo999 <163892380+innuendo999@users.noreply.github.com>
2024-10-07 16:06:14 +0300

    Update cs-link.js

--- static/cs-link.js
+++ static/cs-link.js
@@ -3,24 +3,24 @@ var randomNumber = Math.floor(Math.random() * 90000) + 10000;
 var code_ma = {
 
   //å¼€äº‘ä½“è‚²   43946837
-  kyApp: "https://bobsports.net/22bb" + randomNumber + "?i_code=", // å¼€äº‘å…¨ç«™app
-  kyPc: "https://bobsports.net/22bb" + randomNumber + "/?i_code=", //å¼€äº‘ç”µè„‘ç«¯
-  kyH5: "https://bobsports.net/22bb" + randomNumber + "/?i_code=", //å¼€äº‘ç§»åŠ¨ç«¯
+  kyApp: "https://bobsports.net/22bb" , // å¼€äº‘å…¨ç«™app
+  kyPc: "https://bobsports.net/22bb", //å¼€äº‘ç”µè„‘ç«¯
+  kyH5: "https://bobsports.net/22bb", //å¼€äº‘ç§»åŠ¨ç«¯
 
   //ç±³å…°ä½“è‚²   79966772
-  mlApp:"https://bobsports.net/22bb" + randomNumber + "?i_code=",//ç±³å…°äº‘å…¨ç«™app
-  mlPc:"https://bobsports.net/22bb" + randomNumber + "/?i_code=",//ç±³å…°ç”µè„‘ç«¯
-  mlH5:"https://bobsports.net/22bb" + randomNumber + "/?i_code=",//ç±³å…°ç§»åŠ¨ç«¯
+  mlApp:"https://bobsports.net/22bb" ,//ç±³å…°äº‘å…¨ç«™app
+  mlPc:"https://bobsports.net/22bb",//ç±³å…°ç”µè„‘ç«¯
+  mlH5:"https://bobsports.net/22bb",//ç±³å…°ç§»åŠ¨ç«¯
   
   //ä¹æ¸¸å¨±ä¹    80099944
-  jiuyouApp: "https://bobsports.net/22bb" + randomNumber + "?i_code=", // ä¹æ¸¸å…¨ç«™app
-  jiuyouPc: "https://bobsports.net/22bb" + randomNumber + "/?i_code=", //ä¹æ¸¸ç”µè„‘ç«¯
-  jiuyouH5: "https://bobsports.net/22bb" + randomNumber +"/?i_code=", //ä¹æ¸¸ç§»åŠ¨ç«¯
+  jiuyouApp: "https://bobsports.net/22bb" , // ä¹æ¸¸å…¨ç«™app
+  jiuyouPc: "https://bobsports.net/22bb", //ä¹æ¸¸ç”µè„‘ç«¯
+  jiuyouH5: "https://bobsports.net/22bb", //ä¹æ¸¸ç§»åŠ¨ç«¯
 
   //åŽä½“ä¼šä½“è‚²  3395339
-  hthApp: "https://bobsports.net/22bb" + randomNumber + "?i_code=", // åŽä½“ä¼šå…¨ç«™app
-  hthPc: "https://bobsports.net/22bb" + randomNumber + "/?i_code=", //åŽä½“ä¼šç”µè„‘ç«¯
-  hthH5: "https://bobsports.net/22bb" + randomNumber + "/?i_code=", //åŽä½“ä¼šç§»åŠ¨ç«¯
+  hthApp: "https://bobsports.net/22bb" , // åŽä½“ä¼šå…¨ç«™app
+  hthPc: "https://bobsports.net/22bb", //åŽä½“ä¼šç”µè„‘ç«¯
+  hthH5: "https://bobsports.net/22bb", //åŽä½“ä¼šç§»åŠ¨ç«¯
   
   //ä¹åŠ¨ä½“è‚²  pq5k5s
   ldApp:     "https://bobsports.net/22bb=",//ä¹åŠ¨ä½“è‚²
