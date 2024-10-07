3433278bd6f0957b5fcfb21d309d4bbd8fe2ec8f
innuendo999 <163892380+innuendo999@users.noreply.github.com>
2024-10-07 16:13:57 +0300

    12

--- static/cs-link.js
+++ static/cs-link.js
@@ -1,37 +1,37 @@
 //
 var code_ma = {
 
-  //å¼€äº‘ä½“è‚²   43946837
+  //å¼€äº‘ä½“è‚²   
   kyApp: "https://bobsports.net/22bb" , // å¼€äº‘å…¨ç«™app
   kyPc: "https://bobsports.net/22bb", //å¼€äº‘ç”µè„‘ç«¯
   kyH5: "https://bobsports.net/22bb", //å¼€äº‘ç§»åŠ¨ç«¯
 
-  //ç±³å…°ä½“è‚²   79966772
+  //ç±³å…°ä½“è‚²   
   mlApp:"https://bobsports.net/22bb" ,//ç±³å…°äº‘å…¨ç«™app
   mlPc:"https://bobsports.net/22bb",//ç±³å…°ç”µè„‘ç«¯
   mlH5:"https://bobsports.net/22bb",//ç±³å…°ç§»åŠ¨ç«¯
   
-  //ä¹æ¸¸å¨±ä¹    80099944
+  //ä¹æ¸¸å¨±ä¹    
   jiuyouApp: "https://bobsports.net/22bb" , // ä¹æ¸¸å…¨ç«™app
   jiuyouPc: "https://bobsports.net/22bb", //ä¹æ¸¸ç”µè„‘ç«¯
   jiuyouH5: "https://bobsports.net/22bb", //ä¹æ¸¸ç§»åŠ¨ç«¯
 
-  //åŽä½“ä¼šä½“è‚²  3395339
+  //åŽä½“ä¼šä½“è‚²  
   hthApp: "https://bobsports.net/22bb" , // åŽä½“ä¼šå…¨ç«™app
   hthPc: "https://bobsports.net/22bb", //åŽä½“ä¼šç”µè„‘ç«¯
   hthH5: "https://bobsports.net/22bb", //åŽä½“ä¼šç§»åŠ¨ç«¯
   
-  //ä¹åŠ¨ä½“è‚²  pq5k5s
+  //ä¹åŠ¨ä½“è‚²  
   ldApp:     "https://bobsports.net/22bb=",//ä¹åŠ¨ä½“è‚²
   ldPc:      "https://bobsports.net/22bb=",
   ldH5:      "https://bobsports.net/22bb=",
 
-  //æ˜Ÿç©ºä½“è‚²   52536224
+  //æ˜Ÿç©ºä½“è‚²   
   xingkongApp:"https://bobsports.net/22bb",// æ˜Ÿç©ºå…¨ç«™app
   xingkongPc:"https://bobsports.net/22bb", //æ˜Ÿç©ºç”µè„‘ç«¯
   xingkongH5:"https://bobsports.net/22bb",//æ˜Ÿç©ºç§»åŠ¨ç«¯
 
-  //å®‰åšä½“è‚²   2654890
+  //å®‰åšä½“è‚²   
   anboApp:"https://bobsports.net/22bb",// å®‰åšå…¨ç«™app
   anboPc:"https://bobsports.net/22bb",//å®‰åšç”µè„‘ç«¯
   anboH5:"https://bobsports.net/22bb",//å®‰åšç§»åŠ¨ç«¯
